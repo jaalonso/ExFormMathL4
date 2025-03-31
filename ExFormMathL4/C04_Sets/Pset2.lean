38eb528655de77015f4c1524ef620a6fd9d37db7
Jose A. Alonso <JoseA.Alonso@gmail.com>
2025-03-31 19:43:09 +0200

    Comenta las demostraciones de la relación 4.2

--- ExFormMathL4/C04_Sets/Pset2.lean
+++ ExFormMathL4/C04_Sets/Pset2.lean
@@ -4,7 +4,8 @@
 -- Seville, November 03, 2024
 -- ---------------------------------------------------------------------
 
--- In this problem set, we study results about the universal set and the empty set
+-- In this problem set, we study results about the universal set and the
+-- empty set
 --
 -- It is based on [Section04functions/Sheet2.lean](https://tinyurl.com/2xlkt9kh)
 -- from [Kevin Buzzard's course](https://tinyurl.com/26ek593r).
@@ -19,19 +20,97 @@ variable (x y z : X)
 
 open Set
 
-example : x ∈ (univ : Set X) := by
-  trivial
+-- ---------------------------------------------------------------------
+-- Ejercicio 1. Demostrar que
+--    x ∈ univ
+-- ---------------------------------------------------------------------
 
-example : x ∈ (∅ : Set X) → False := by
+example : x ∈ (univ : Set X) :=
+by trivial
+
+-- ---------------------------------------------------------------------
+-- Ejercicio 2. Demostrar que
+--    x ∉ ∅
+-- ---------------------------------------------------------------------
+
+-- 1ª demostración
+-- ===============
+
+example : x ∉ (∅ : Set X) :=
+by
   intro h
+  -- h : x ∈ ∅
+  -- ⊢ False
   cases h
 
-example : A ⊆ univ := by
+-- 2ª demostración
+-- ===============
+
+example : x ∉ (∅ : Set X) :=
+by exact id
+
+-- 3ª demostración
+-- ===============
+
+example : x ∉ (∅ : Set X) :=
+by aesop
+
+-- ---------------------------------------------------------------------
+-- Ejercicio 3. Demostrar que
+--    A ⊆ univ
+-- ---------------------------------------------------------------------
+
+-- 1ª demostración
+-- ===============
+
+example : A ⊆ univ :=
+by
   rw [subset_def]
+  -- ⊢ ∀ x ∈ A, x ∈ univ
   intro x _hxA
+  -- x : X
+  -- _hxA : x ∈ A
+  -- ⊢ x ∈ univ
   trivial
 
-example : ∅ ⊆ A := by
+-- 2ª demostración
+-- ===============
+
+example : A ⊆ univ :=
+fun _ _ => trivial
+
+-- 3ª demostración
+-- ===============
+
+example : A ⊆ univ :=
+by aesop
+
+-- ---------------------------------------------------------------------
+-- Ejercicio 4. Demostrar que
+--    ∅ ⊆ A
+-- ---------------------------------------------------------------------
+
+-- 1ª demostración
+-- ===============
+
+example : ∅ ⊆ A :=
+by
   rw [subset_def]
+  -- ⊢ ∀ x ∈ ∅, x ∈ A
   intro x hx
+  -- x : X
+  -- hx : x ∈ ∅
+  -- ⊢ x ∈ A
   cases hx
+
+-- 2ª demostración
+-- ===============
+
+example : ∅ ⊆ A :=
+empty_subset A
+
+-- 3ª demostración
+-- ===============
+
+example : ∅ ⊆ A :=
+by aesop
